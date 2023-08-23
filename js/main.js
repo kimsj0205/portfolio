153bd4aea39a7a31a3b212db555946a6f2cf872d
kimsj0205 <kkeeyy0205@naver.com>
2023-08-23 11:11:43 +0900

    ver "1.0.1"

--- js/main.js
+++ js/main.js
@@ -3,7 +3,39 @@
 //   alert("1920 * 1080 화면에 최적화 되어 있습니다.");
 // });
 
+// popup 1
+
+$(function(){
+  //id가 gallery인 노드의 a태그에 lightBox를 적용 시킨다
+    $("#popup_wrap4 a").lightBox();
+  });
+$(function(){
+    $("#popup_wrap4 a").lightBox({
+     //배경 색 지정
+      overlayBgColor:"#f00",
+     //이미지가 선택되고, 흑백으로 처리 되는 주변의 불투명도 조절
+      overlayOpacity:0.3,
+     //이미지가 확대되는 시간
+      containerResizeSpeed:100
+    });
+});
 // popup
+
+
+// 1번째 팝업
+$('.design_img ul li').eq(0).click(function(){
+  $('.popup_wrap1').fadeIn();
+});
+$('.popup_wrap1').click(function(){
+  $('.popup_wrap1').fadeOut();
+});
+// 2번째 팝업
+$('.design_img ul li').eq(1).click(function(){
+  $('.popup_wrap2').fadeIn();
+});
+$('.popup_wrap2').click(function(){
+  $('.popup_wrap2').fadeOut();
+});
 
 // 3번째 팝업
 $('.design_img ul li').eq(2).click(function(){
@@ -21,6 +53,15 @@ $('.design_img ul li').eq(3).click(function(){
 $('.popup_wrap3').click(function(){
   $('.popup_wrap3').fadeOut();
 });
+
+// 5번째 팝업
+$('.design_img ul li').eq(4).click(function(){
+  $('.popup_wrap5').fadeIn();
+});
+$('.popup_wrap5').click(function(){
+  $('.popup_wrap5').fadeOut();
+});
+
 
 // 메뉴 클릭 시 이동
 
@@ -44,3 +85,32 @@ $('main .fig4 .circle .contact .profileimg').mouseenter(function(){
 $('.b_circle').mouseleave(function(){
   $(this).fadeOut();
 });
+
+// chart
+$(window).ready(function(){
+  draw(90, '.pie-chart1', '#5474b0');
+  draw(80, '.pie-chart2', '#ff8838');
+  draw(75, '.pie-chart3', '#8a55d3');
+  draw(75, '.pie-chart4', '#df3b3b');
+  draw(70, '.pie-chart5', '#54b7d3');
+  draw(30, '.pie-chart6','#ff4e00');
+});
+
+function draw(max, classname, colorname){
+  var i=1;
+    var func1 = setInterval(function(){
+      if(i<max){
+          color1(i,classname,colorname);
+          i++;
+      } else{
+        clearInterval(func1);
+      }
+    },10);
+}
+function color1(i, classname,colorname){
+  $(classname).css({
+        "background":"conic-gradient("+colorname+" 0% "+i+"%, #ffffff "+i+"% 100%)"
+  });
+}
+
+
